c1283e7ed2f9769e9f248352748c8e25ff70b6da
suillno <shtndlf46@gmail.com>
2025-05-12 17:00:30 +0900

    테스트
    테스트

---
+++ yagaja/src/main/java/com/trip/webpage/YagajaApplication.java
@@ -0,0 +1,13 @@
+package com.trip.webpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YagajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YagajaApplication.class, args);
+	}
+
+}
