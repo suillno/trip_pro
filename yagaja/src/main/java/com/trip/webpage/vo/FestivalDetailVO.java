45d930f103f075cb61abdbabbcdb3b1d0725d223
suillno <shtndlf46@gmail.com>
2025-05-22 15:10:35 +0900

    json 호출 및 축제정보 출력
    json 호출 및 축제정보 출력

---
+++ yagaja/src/main/java/com/trip/webpage/vo/FestivalDetailVO.java
@@ -0,0 +1,43 @@
+package com.trip.webpage.vo;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class FestivalDetailVO {
+
+		private String contentid;
+
+		private String title;
+
+		private String createdTime;
+
+		private String modifiedTime;
+
+		private String tel;
+
+		private String telName;
+
+		private String homepage;
+
+		private String firstImage;
+
+		private String firstImage2;
+
+		private String addr1;
+
+		private String addr2;
+
+		private String mapx;
+
+		private String mapy;
+
+		private String overview;
+
+}
